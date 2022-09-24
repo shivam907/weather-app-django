e19de5dc1be89449128072f148a75e12971ac651
shivam907 <shivamkaushal907@gmail.com>
2022-09-24 18:45:58 +0530

    An update

--- weather/settings.py
+++ weather/settings.py
@@ -123,4 +123,4 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-ALLOWED_HOSTS = ['weather-app-cez.herokuapp.com']
+ALLOWED_HOSTS = ['weather-app-cez.herokuapp.com','localhost']
